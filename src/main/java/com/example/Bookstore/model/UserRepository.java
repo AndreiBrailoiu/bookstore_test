05b347e7123fc50aac75e933218eb4373821e9d2
AndreiBrailoiu <darkyss94@gmail.com>
2022-11-24 08:51:59 +0200

    pom file fixed

--- src/main/java/com/example/Bookstore/model/UserRepository.java
+++ src/main/java/com/example/Bookstore/model/UserRepository.java
@@ -2,6 +2,6 @@ package com.example.Bookstore.model;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<UserEntity, Long>{
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
 	UserEntity findByUsername(String username);
 }
